631efe3b9198686b936b3392b89a0384e1fa0ece
Matthew Basanta <matthew.basanta@gmail.com>
2015-07-29 21:57:35 -0400

    Update Gruntfile to rename app

--- website/Gruntfile.js
+++ website/Gruntfile.js
@@ -319,7 +319,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'websiteApp',
+          module: 'projectiveDetectiveApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
